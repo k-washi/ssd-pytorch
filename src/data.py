cdd7f8bc879b90c0cf955705697072b237b56550
k-washi <kai.washizaki@gmail.com>
2020-02-14 23:58:06 +0900

    Data set creater

--- src/data.py
+++ src/data.py
@@ -3,8 +3,25 @@ import sys
 
 sys.path.append(os.path.join(os.path.dirname(__file__), '..'))
 
-from src._setting import *
+import os.path as osp
+import random
+# XMLをファイルやテキストから読み込んだり、加工したり、保存したりするためのライブラリ
+import xml.etree.ElementTree as ET
 
+import cv2
+import matplotlib.pyplot as plt
+import numpy as np
+import torch
+import torch.utils.data as data
+
+from utils.data_augumentation import Compose, ConvertFromInts, ToAbsoluteCoords, PhotometricDistort, Expand, RandomSampleCrop, RandomMirror, ToPercentCoords, Resize, SubtractMeans
+
+torch.manual_seed(1234)
+np.random.seed(1234)
+random.seed(1234)
+
+
+# アノテーションの追加
 def make_datapath_list(rootpath):
     """
     データへのパスを格納したリストを作成する。
@@ -52,8 +69,255 @@ def make_datapath_list(rootpath):
 
     return train_img_list, train_anno_list, val_img_list, val_anno_list
 
-if __name__ == "__main__":
-  rootpath = "/Users/washizakikai/data/VOCdevkit/VOC2012/"
-  train_img_list, train_anno_list, val_img_list, val_anno_list = make_datapath_list(rootpath)
 
-  print(train_img_list[0])
+class Anno_xml2list(object):
+    def __init__(self, classes):
+
+        self.classes = classes
+
+    def __call__(self, xml_path, width, height):
+        """
+        1枚の画像に対する「XML形式のアノテーションデータ」を、画像サイズで規格化してからリスト形式に変換する。
+
+        Parameters
+        ----------
+        xml_path : str
+            xmlファイルへのパス。
+        width : int
+            対象画像の幅。
+        height : int
+            対象画像の高さ。
+
+        Returns
+        -------
+        ret : [[xmin, ymin, xmax, ymax, label_ind], ... ]
+            物体のアノテーションデータを格納したリスト。画像内に存在する物体数分のだけ要素を持つ。
+        """
+
+        # 画像内の全ての物体のアノテーションをこのリストに格納します
+        ret = []
+
+        # xmlファイルを読み込む
+        xml = ET.parse(xml_path).getroot()
+
+        # 画像内にある物体（object）の数だけループする
+        for obj in xml.iter('object'):
+
+            # アノテーションで検知がdifficultに設定されているものは除外
+            difficult = int(obj.find('difficult').text)
+            if difficult == 1:
+                continue
+
+            # 1つの物体に対するアノテーションを格納するリスト
+            bndbox = []
+
+            name = obj.find('name').text.lower().strip()  # 物体名
+            bbox = obj.find('bndbox')  # バウンディングボックスの情報
+
+            # アノテーションの xmin, ymin, xmax, ymaxを取得し、0～1に規格化
+            pts = ['xmin', 'ymin', 'xmax', 'ymax']
+
+            for pt in (pts):
+                # VOCは原点が(1,1)なので1を引き算して（0, 0）に
+                cur_pixel = int(bbox.find(pt).text) - 1
+
+                # 幅、高さで規格化
+                if pt == 'xmin' or pt == 'xmax':  # x方向のときは幅で割算
+                    cur_pixel /= width
+                else:  # y方向のときは高さで割算
+                    cur_pixel /= height
+
+                bndbox.append(cur_pixel)
+
+            # アノテーションのクラス名のindexを取得して追加
+            label_idx = self.classes.index(name)
+            bndbox.append(label_idx)
+
+            # resに[xmin, ymin, xmax, ymax, label_ind]を足す
+            ret += [bndbox]
+
+        return np.array(ret)  # [[xmin, ymin, xmax, ymax, label_ind], ... ]
+class DataTransform():
+    """
+    画像とアノテーションの前処理クラス。訓練と推論で異なる動作をする。
+    画像のサイズを300x300にする。
+    学習時はデータオーギュメンテーションする。
+
+
+    Attributes
+    ----------
+    input_size : int
+        リサイズ先の画像の大きさ。
+    color_mean : (B, G, R)
+        各色チャネルの平均値。
+    """
+
+    def __init__(self, input_size, color_mean):
+        self.data_transform = {
+            'train': Compose([
+                ConvertFromInts(),  # intをfloat32に変換
+                ToAbsoluteCoords(),  # アノテーションデータの規格化を戻す
+                PhotometricDistort(),  # 画像の色調などをランダムに変化
+                Expand(color_mean),  # 画像のキャンバスを広げる
+                RandomSampleCrop(),  # 画像内の部分をランダムに抜き出す
+                RandomMirror(),  # 画像を反転させる
+                ToPercentCoords(),  # アノテーションデータを0-1に規格化
+                Resize(input_size),  # 画像サイズをinput_size×input_sizeに変形
+                SubtractMeans(color_mean)  # BGRの色の平均値を引き算
+            ]),
+            'val': Compose([
+                ConvertFromInts(),  # intをfloatに変換
+                Resize(input_size),  # 画像サイズをinput_size×input_sizeに変形
+                SubtractMeans(color_mean)  # BGRの色の平均値を引き算
+            ])
+        }
+
+    def __call__(self, img, phase, boxes, labels):
+        """
+        Parameters
+        ----------
+        phase : 'train' or 'val'
+            前処理のモードを指定。
+        """
+        return self.data_transform[phase](img, boxes, labels)
+
+#Create Dataset
+class VOCDataset(data.Dataset):
+    """
+    VOC2012のDatasetを作成するクラス。PyTorchのDatasetクラスを継承。
+
+    Attributes
+    ----------
+    img_list : リスト
+        画像のパスを格納したリスト
+    anno_list : リスト
+        アノテーションへのパスを格納したリスト
+    phase : 'train' or 'test'
+        学習か訓練かを設定する。
+    transform : object
+        前処理クラスのインスタンス
+    transform_anno : object
+        xmlのアノテーションをリストに変換するインスタンス
+    """
+
+    def __init__(self, img_list, anno_list, phase, transform, transform_anno):
+        self.img_list = img_list
+        self.anno_list = anno_list
+        self.phase = phase  # train もしくは valを指定
+        self.transform = transform  # 画像の変形
+        self.transform_anno = transform_anno  # アノテーションデータをxmlからリストへ
+
+    def __len__(self):
+        '''画像の枚数を返す'''
+        return len(self.img_list)
+
+    def __getitem__(self, index):
+        '''
+        前処理をした画像のテンソル形式のデータとアノテーションを取得
+        '''
+        im, gt, h, w = self.pull_item(index)
+        return im, gt
+
+    def pull_item(self, index):
+        '''前処理をした画像のテンソル形式のデータ、アノテーション、画像の高さ、幅を取得する'''
+
+        # 1. 画像読み込み
+        image_file_path = self.img_list[index]
+        img = cv2.imread(image_file_path)  # [高さ][幅][色BGR]
+        height, width, channels = img.shape  # 画像のサイズを取得
+
+        # 2. xml形式のアノテーション情報をリストに
+        anno_file_path = self.anno_list[index]
+        anno_list = self.transform_anno(anno_file_path, width, height)
+
+        # 3. 前処理を実施
+        img, boxes, labels = self.transform(
+            img, self.phase, anno_list[:, :4], anno_list[:, 4])
+
+        # 色チャネルの順番がBGRになっているので、RGBに順番変更
+        # さらに（高さ、幅、色チャネル）の順を（色チャネル、高さ、幅）に変換
+        img = torch.from_numpy(img[:, :, (2, 1, 0)]).permute(2, 0, 1)
+
+        # BBoxとラベルをセットにしたnp.arrayを作成、変数名「gt」はground truth（答え）の略称
+        gt = np.hstack((boxes, np.expand_dims(labels, axis=1)))
+
+        return img, gt, height, width
+
+
+if __name__ == "__main__":
+    rootpath = "/Users/washizakikai/data/VOCdevkit/VOC2012/"
+    train_img_list, train_anno_list, val_img_list, val_anno_list = make_datapath_list(rootpath)
+
+    print(train_img_list[0])
+
+    # 動作確認　
+    voc_classes = ['aeroplane', 'bicycle', 'bird', 'boat',
+                'bottle', 'bus', 'car', 'cat', 'chair',
+                'cow', 'diningtable', 'dog', 'horse',
+                'motorbike', 'person', 'pottedplant',
+                'sheep', 'sofa', 'train', 'tvmonitor']
+
+    transform_anno = Anno_xml2list(voc_classes)
+
+    # 画像の読み込み OpenCVを使用
+    ind = 1
+    image_file_path = val_img_list[ind]
+    img = cv2.imread(image_file_path)  # [高さ][幅][色BGR]
+    height, width, channels = img.shape  # 画像のサイズを取得
+
+    # アノテーションをリストで表示
+    print(val_anno_list[ind])
+    anno = transform_anno(val_anno_list[ind], width, height)
+    print(anno)
+
+    # 動作の確認
+
+    # 1. 画像読み込み
+    image_file_path = train_img_list[0]
+    img = cv2.imread(image_file_path)  # [高さ][幅][色BGR]
+    height, width, channels = img.shape  # 画像のサイズを取得
+
+    # 2. アノテーションをリストに
+    transform_anno = Anno_xml2list(voc_classes)
+    print(transform_anno)
+    anno_list = transform_anno(train_anno_list[0], width, height)
+
+    # 3. 元画像の表示
+    #plt.imshow(cv2.cvtColor(img, cv2.COLOR_BGR2RGB))
+    #plt.show()
+
+    # 4. 前処理クラスの作成
+    color_mean = (104, 117, 123)  # (BGR)の色の平均値
+    input_size = 300  # 画像のinputサイズを300×300にする
+    transform = DataTransform(input_size, color_mean)
+
+    # 5. train画像の表示
+    phase = "train"
+    img_transformed, boxes, labels = transform(
+        img, phase, anno_list[:, :4], anno_list[:, 4])
+    #plt.imshow(cv2.cvtColor(img_transformed, cv2.COLOR_BGR2RGB))
+    #plt.show()
+
+
+    # 6. val画像の表示
+    phase = "val"
+    img_transformed, boxes, labels = transform(
+        img, phase, anno_list[:, :4], anno_list[:, 4])
+    #plt.imshow(cv2.cvtColor(img_transformed, cv2.COLOR_BGR2RGB))
+    #plt.show()
+
+    #print(boxes)
+
+    # 動作確認
+    color_mean = (104, 117, 123)  # (BGR)の色の平均値
+    input_size = 300  # 画像のinputサイズを300×300にする
+
+    train_dataset = VOCDataset(train_img_list, train_anno_list, phase="train", transform=DataTransform(
+        input_size, color_mean), transform_anno=Anno_xml2list(voc_classes))
+
+    val_dataset = VOCDataset(val_img_list, val_anno_list, phase="val", transform=DataTransform(
+        input_size, color_mean), transform_anno=Anno_xml2list(voc_classes))
+
+
+    # データの取り出し例
+    print(val_dataset.__getitem__(1))
